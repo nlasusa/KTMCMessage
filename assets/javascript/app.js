bcb58755397e14fce39d98be392f603e677062c2
Nicole LaSusa <nicolelasusa@gmail.com>
2020-06-08 17:30:45 -0400

    updated data-aos

---
+++ assets/javascript/app.js
@@ -0,0 +1,23 @@
+/*--- ON PAGE LOAD THIS FUNCTION WILL DISPLAY A POP UP MESSAGE ADVERTISING A SPECIAL WINE CLUB DEAL ---*/
+function displayAd(){
+    document.getElementById("special-offer").style.display = "block";
+}
+window.addEventListener('load', displayAd, false);
+
+
+/*--- THIS FUNCTION WILL CLEAR THE POP-UP WHEN THE CLOSE BUTTON IS CLICKED ---*/
+document.getElementsByClassName("close-button")[0].onclick = function() {
+    document.getElementById("special-offer").style.display = "none";
+}
+
+/*--- THIS FUNCTION WILL CLEAR THE POP-UP FORM WHEN THE CLOSE BUTTON IS CLICKED ---*/
+document.getElementsByClassName("close-button")[1].onclick = function() {
+    document.getElementById("form-container").style.display = "none";
+}
+
+
+/*--- THIS FUNCTION WILL OPEN THE REGISTRATION FORM AND CLOSE THE POPUP ---*/
+document.getElementById("signup-btn").addEventListener('click', function(){
+    document.getElementById("special-offer").style.display = "none";
+    document.getElementById("form-container").style.display = "flex";
+}, false)
